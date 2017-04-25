8db26ba93ee2587ff6fa56a9fd67c97bdca0a1e1
calkan <calkan@gmail.com>
2017-04-25 22:34:15 +0300

    interval search done.

---
+++ sonic_structures.h
@@ -0,0 +1,56 @@
+#ifndef __SONIC_STRUCTURES
+#define __SONIC_STRUCTURES
+
+
+typedef enum SONIC_INTERVAL_TYPE {GAP, DUP, REP} sonic_interval_type;
+
+typedef struct _sonic_repeat
+{
+  char strand;
+  char *repeat_type;
+  char *repeat_class;
+  int repeat_start, repeat_end;
+} sonic_repeat;
+
+
+typedef struct sonic_interval_linked_list
+{
+  int start;
+  int end;
+  struct _sonic_repeat *repeat_item;
+  struct _interval_linked_list *next;
+} sonic_interval_linked_list;
+
+typedef struct _sonic_interval
+{
+  int start;
+  int end;
+  struct _sonic_repeat *repeat_item;
+} sonic_interval;
+
+
+typedef struct _sonic_bed_line
+{
+  char chromosome[255];
+  int start;
+  int end;
+  struct _sonic_repeat *repeat_item;
+} sonic_bed_line;
+
+typedef struct _sonic
+{
+  int number_of_chromosomes;
+  int *number_of_gaps_in_chromosome;
+  int *number_of_dups_in_chromosome;
+  int *number_of_repeats_in_chromosome;
+  int *chromosome_lengths;
+  long genome_length;
+  char **chromosome_gc_profile;
+  char **chromosome_names;
+  struct _sonic_interval **gaps;
+  struct _sonic_interval **dups;
+  struct _sonic_interval **reps;
+} sonic;
+
+
+#endif
